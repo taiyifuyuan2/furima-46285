e6042435856ac0c62e7248d860a3226c3430a8c9
taiyifuyuan2 <taiyifuyuan2@gmail.com>
2025-08-20 20:23:27 +0900

    追加レビュー指摘の修正完了
    ## What（何を修正したか）
    - カスタムコントローラーの完全削除（Users::RegistrationsController）
    - routes.rbのcontrollersオプション削除
    - パラメータ許可処理をapplication_controller.rbに集約
    
    ## Why（なぜこの修正が必要だったか）
    - Deviseのデフォルト機能で十分
    - 重複処理の解消
    - コードの簡素化と保守性向上

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -9,5 +10,10 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == '2222'
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[nickname last_name first_name last_name_kana first_name_kana birth_date])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[nickname last_name first_name last_name_kana first_name_kana birth_date])
   end
 end
