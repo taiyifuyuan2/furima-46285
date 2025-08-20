8eeebdb21a3984ceb4088fc237cee0e8e04194fe
taiyifuyuan2 <taiyifuyuan2@gmail.com>
2025-08-20 20:02:24 +0900

    レビュー指摘の修正完了
    ## What（何を修正したか）
    - 不要なカスタムコントローラーの削除
    - データベースマイグレーションにnull: false制約を追加
    - テストファイルの再作成
    
    ## Why（なぜこの修正が必要だったか）
    - 課題の要件に合わせた実装
    - データの整合性向上
    - コードの簡素化

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,57 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(
+      email: 'test@example.com',
+      password: 'password123',
+      password_confirmation: 'password123',
+      nickname: 'テストユーザー',
+      last_name: '田中',
+      first_name: '太郎',
+      last_name_kana: 'タナカ',
+      first_name_kana: 'タロウ',
+      birth_date: Date.new(1990, 1, 1)
+    )
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+  end
+
+  test 'nickname should be present' do
+    @user.nickname = nil
+    assert_not @user.valid?
+  end
+
+  test 'last_name should be present' do
+    @user.last_name = nil
+    assert_not @user.valid?
+  end
+
+  test 'first_name should be present' do
+    @user.first_name = nil
+    assert_not @user.valid?
+  end
+
+  test 'last_name_kana should be present' do
+    @user.last_name_kana = nil
+    assert_not @user.valid?
+  end
+
+  test 'first_name_kana should be present' do
+    @user.first_name_kana = nil
+    assert_not @user.valid?
+  end
+
+  test 'birth_date should be present' do
+    @user.birth_date = nil
+    assert_not @user.valid?
+  end
+
+  test 'password should include both letters and numbers' do
+    @user.password = 'password'
+    @user.password_confirmation = 'password'
+    assert_not @user.valid?
+  end
 end
