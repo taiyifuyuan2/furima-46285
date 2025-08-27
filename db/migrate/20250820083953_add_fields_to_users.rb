728a8ec9014f47cb692004bc61c1c54df8451898
taiyifuyuan2 <taiyifuyuan2@gmail.com>
2025-08-27 19:08:24 +0900

    マイグレーションファイルの最終更新

--- db/migrate/20250820083953_add_fields_to_users.rb
+++ db/migrate/20250820083953_add_fields_to_users.rb
@@ -3,7 +3,6 @@
 class AddFieldsToUsers < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :nickname, :string, null: false
-    add_column :users, :email, :string, null: false
     add_column :users, :encrypted_password, :string, null: false
     add_column :users, :last_name, :string, null: false
     add_column :users, :first_name, :string, null: false
