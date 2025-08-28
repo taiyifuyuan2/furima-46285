4acf4467d9c02c997a6f02cd9518bf1723d4b27f
taiyifuyuan2 <taiyifuyuan2@gmail.com>
2025-08-28 17:48:37 +0900

    適当なコメントを追加：テスト用の変更

--- db/migrate/20250820083037_devise_create_users.rb
+++ db/migrate/20250820083037_devise_create_users.rb
@@ -4,8 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ''
-      t.string :encrypted_password, null: false, default: ''
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
@@ -41,3 +41,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
     # add_index :users, :unlock_token,         unique: true
   end
 end
+
+# 適当なコメントを追加
+# テスト用の変更
+# コミット用の文字列
