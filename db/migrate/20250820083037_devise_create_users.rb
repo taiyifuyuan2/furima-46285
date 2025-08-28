f774c6dec19f3c1cc29eaf2e70fad1c1730f90a3
taiyifuyuan2 <taiyifuyuan2@gmail.com>
2025-08-28 17:56:24 +0900

    Deviseマイグレーションファイルの更新：limit: 191の指定とインデックス設定を追加

--- db/migrate/20250820083037_devise_create_users.rb
+++ db/migrate/20250820083037_devise_create_users.rb
@@ -4,11 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false, default: "", limit: 191
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
+      t.string   :reset_password_token, limit: 191
       t.datetime :reset_password_sent_at
 
       ## Rememberable
@@ -22,27 +22,29 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       # t.string   :last_sign_in_ip
 
       ## Confirmable
-      # t.string   :confirmation_token
+      # t.string   :confirmation_token, limit: 191
       # t.datetime :confirmed_at
       # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      # t.string   :unconfirmed_email
 
       ## Lockable
-      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
+      # t.integer  :failed_attempts, default: 0, null: false
+      # t.string   :unlock_token, limit: 191
       # t.datetime :locked_at
 
       t.timestamps null: false
     end
 
-    t.string :email,              null: false, default: "", limit: 191
-    t.string :reset_password_token, limit: 191
-    
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    # インデックス（長さ 191 を指定）
+    add_index :users, :email,                unique: true, length: 191
+    add_index :users, :reset_password_token, unique: true, length: 191
+    # add_index :users, :confirmation_token,  unique: true, length: 191   # 使うなら
+    # add_index :users, :unlock_token,        unique: true, length: 191   # 使うなら
   end
 end
+
 
 # 適当なコメントを追加
 # テスト用の変更
 # コミット用の文字列
+# 最終更新完了
